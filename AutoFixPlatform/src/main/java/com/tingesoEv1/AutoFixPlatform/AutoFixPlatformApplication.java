84f56d6f8c6a89430c7bd6e0f4f8a2e34d71f46e
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-04-03 13:09:12 -0300

    Spring

---
+++ AutoFixPlatform/src/main/java/com/tingesoEv1/AutoFixPlatform/AutoFixPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tingesoEv1.AutoFixPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoFixPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoFixPlatformApplication.class, args);
+	}
+
+}
---
+++ AutoFixPlatform/src/test/java/com/tingesoEv1/AutoFixPlatform/AutoFixPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tingesoEv1.AutoFixPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoFixPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
