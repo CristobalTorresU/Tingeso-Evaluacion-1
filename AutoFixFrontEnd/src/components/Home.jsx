04ddaab884605bbfebaec2a3cd0fd18f2b6c4954
CristobalTorresU <cristobal.torres.u@usach.cl>
2024-05-01 15:06:32 -0400

    Update Front

--- AutoFixFrontEnd/src/components/Home.jsx
+++ AutoFixFrontEnd/src/components/Home.jsx
@@ -1,13 +1,14 @@
 const Home = () => {
     return (
         <div>
+            <br/>
+            <br/>
+            <br/>
             <h1>AutoFix: Reparamos su Auto en un Flash</h1>
             <p>
                 AutoFix funciona registrando vehículos en nuestra base de datos
                 para posteriormente poder cotizar reparaciones de acuerdo a distintos
                 parámetros del vehículo.
-                Para más información, visite{" "}
-                <a href="https://www.gnu.org/">GNU</a>. 
                 Saludos.
             </p>
         </div>
--- AutoFixFrontEnd/src/components/NotFound.jsx
+++ AutoFixFrontEnd/src/components/NotFound.jsx
@@ -1,10 +1,12 @@
-import surprised from '../../images/surprised.png';
 import '../App.css';
 
 const NotFound = () => {
     return (
         <div>
-            <img src={surprised} alt="Sorprendida" className="rotating-image" />
+            <br />
+            <br />
+            <br />
+            <br />
             <h1>La dirección URL a la que ingresó no existe.</h1>
         </div>
     );
